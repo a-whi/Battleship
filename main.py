a4b65fd9cec081a7c9c8c0dc7310ba43bff66e45
ALEXANDER_WHITFIELD <awhi0036@student.monash.edu>
2023-10-12 11:44:43 +0100

    started

---
+++ main.py
@@ -0,0 +1,53 @@
+"""
+Created by: Alex Whitfield
+Battle ship main file
+
+Rules I am following:
+- Boats:
+    - 1 x 5 size boat
+    - 1 x 4 size boat
+    - 2 x 3 size boat
+    - 1 x 1 size boat
+- Boats can be placed anywhere on boat, even next to each other
+
+Method:
+- We will use a probablility map, one will be created for all boats, then will be added
+  together. 
+- As we fire the maps will be updated
+- When a ship is sunk we record what type of ship it is so we dont have to look for it.
+
+"""
+
+grid_size = 10
+hitTarget = [] # This will record grid positions for where we have hit a target
+
+def main():
+    if len(hitTarget) != 0:
+        # If the list isn't empty it means last shot we hit a boat and need to search for it
+        # Check up, down, left right 
+        print()
+    else:
+        print(2,1)
+        # Find highest probability and say it
+    
+# Create a probabilty map for each boat
+def probMap():
+    map5 = []
+    for i in range(grid_size):
+        row = []
+        for j in range(grid_size):
+            row.append(False)
+
+        map5.append(row)
+
+def grid_creation(grid_size):
+    grid = []
+    for i in range(grid_size):
+        row = []
+        for j in range(grid_size):
+            row.append(False)
+
+        grid.append(row)
+
+main()
+
