e1d5cba13cd827ae5323866f9d34cf1fe1f969cd
ALEXANDER_WHITFIELD <awhi0036@student.monash.edu>
2023-10-14 18:55:09 +0100

    ,

--- main.py
+++ main.py
@@ -18,10 +18,20 @@ Method:
 
 """
 
+# Global values
 grid_size = 10
 hitTarget = [] # This will record grid positions for where we have hit a target
+boatmap = []
 
+##################################################################################################
 def main():
+
+    # Create tracking grid
+    trackingGrid_creation()
+
+    # Create ship probability map
+    probMap()       #Work on the grid so it can update and work for all boat sizes
+
     if len(hitTarget) != 0:
         # If the list isn't empty it means last shot we hit a boat and need to search for it
         # Check up, down, left right 
@@ -29,10 +39,22 @@ def main():
     else:
         print(2,1)
         # Find highest probability and say it
+
+##################################################################################################
+# All to do with the probability map
+def function_h(i, j, boat_length):
+    z = 0
+    while z < boat_length:
+        boatmap[i][j+z] += 1
+        z+=1
     
+def function_v(i, j, boat_length):
+    z = 0
+    while z < boat_length:
+        boatmap[i+z][j] += 1
+        z+=1
 # Create a probabilty map for each boat
 def probMap():
-    boatmap = []
     boat_length = 5
     for i in range(grid_size):
         row = []
@@ -41,27 +63,33 @@ def probMap():
 
         boatmap.append(row)
 
-    for i in range(grid_size):      # Not adding 1 each time just making the value = 1
-        row = []
+    for i in range(grid_size):   
         for j in range(grid_size):
-            if i+boat_length <= grid_size:
-                boatmap[i][j]+=1
-            elif j+boat_length <= grid_size:
-                boatmap[i][j] +=1
+            if j+boat_length <= grid_size:
+                function_h(i, j, boat_length)
+                
+                if i+boat_length <= grid_size:
+                    function_v(i, j, boat_length)
+                    
+            elif i+boat_length <= grid_size:
+                function_v(i, j, boat_length)
+                
+                if j+boat_length <= grid_size:
+                    function_h(i, j, boat_length)
             else:
                 continue
     print(boatmap)
+##################################################################################################
 
-
-def grid_creation(grid_size):
-    grid = []
+# This is a grid where every element is True or False, keeps track of where we have fired
+def trackingGrid_creation(grid_size):
+    trackingGrid = []
     for i in range(grid_size):
         row = []
         for j in range(grid_size):
             row.append(False)
 
-        grid.append(row)
+        trackingGrid.append(row)
 
-probMap()
 main()
 
